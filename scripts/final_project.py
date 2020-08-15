654a483a7a62f291b695fc0fff5b7c30ed868b56
Brian Bock <bbock@terpmail.umd.edu>
2020-08-15 18:01:35 -0400

    modified go_to_goal. Increased turn tolerance. Started hug wall FR/BR checks(not done). Need to improve go_to_goal control function

--- scripts/final_project.py
+++ scripts/final_project.py
@@ -117,7 +117,7 @@ def go_to_goal(goal):
         elif last_rotation < -math.pi + 0.1 and rotation > 0:
             rotation = -2 * math.pi + rotation
 
-        velocity_msg.angular.z = k_v_gain * angle_to_goal - rotation
+        velocity_msg.angular.z = k_v_gain * (angle_to_goal - rotation)
         distance_to_goal = compute_dist(position.x, position.y, goal_x, goal_y)
         velocity_msg.linear.x = min(k_h_gain * distance_to_goal, .5)
 
@@ -138,7 +138,6 @@ def go_to_goal(goal):
     return True
 
 
-
 def drive():
     velocity_msg.linear.x = 1
     pub.publish(velocity_msg)
@@ -155,12 +154,12 @@ def turn(angle):
     goal_angle = rotation + angle
     # print(math.degrees(angle_to_goal))
 
-    while goal_angle - rotation > .008726646:  # half a degree, in radians
+    while goal_angle - rotation > math.radians(1):
         velocity_msg.angular.z = k_v_gain * goal_angle - rotation
         if velocity_msg.angular.z > 0:
-            velocity_msg.angular.z = min(velocity_msg.angular.z, 1.5)
+            velocity_msg.angular.z = min(velocity_msg.angular.z, 1)
         else:
-            velocity_msg.angular.z = max(velocity_msg.angular.z, -1.5)
+            velocity_msg.angular.z = max(velocity_msg.angular.z, -1)
         pub.publish(velocity_msg)
         (position, rotation) = get_odom_data()
     velocity_msg.angular.z = 0.0
@@ -181,8 +180,8 @@ def go_to_wall(angle):
     front = scan.ranges[0]
     clearance = .2
     (position, rotation) = get_odom_data()
-    goaly = math.sin(rotation)*(front-clearance) + position.y
-    goalx = math.cos(rotation)*(front-clearance) + position.x
+    goaly = math.sin(rotation) * (front - clearance) + position.y
+    goalx = math.cos(rotation) * (front - clearance) + position.x
     print("Current position: {x},{y}".format(x=position.x, y=position.y))
     print("Goal position: {x},{y}".format(x=goalx, y=goaly))
     go_to_goal((goalx, goaly))
@@ -197,18 +196,38 @@ def go_to_wall(angle):
     velocity_msg.angular.z = 0
     pub.publish(velocity_msg)
     # rotate 90 CCW
-    turn(math.pi/2)
+    turn(math.pi / 2)
     # Save position for obstacle check
     wall_start = get_odom_data()[0]
-    return wall_start
+    return wall_start, clearance
 
 
-def hug_wall(wall_start):
+def hug_wall(wall_start, clearance):
     """ This is the core of the wall follower algorithm. The robot drives straight with the wall a preset distance to
     it's right. """
     print("Wall following time!")
     success = False
+    velocity_msg.linear.x = .3
+    pub.publish(velocity_msg)
+    F, FR, R, BR = right_scan()
+    print("Distances:")
+    print("Front {f}, FR {fr}, R {r}, BR {br}".format(f=F, fr=FR, r=R, br=BR))
+    direction = 1
+    angle_dist = clearance / math.cos(math.radians(15))  # +.1
+    theta = math.acos(R / FR)  # Angle between the horizontal and angle of FR
+    phi = math.acos(R / BR)
+    print("Theta is {t}".format(t=math.degrees(theta)))
+    print("Phi is {p}".format(p=math.degrees(phi)))
+    while (BR > angle_dist) or (FR > angle_dist):
+        if FR - BR > .1:
+            # Robot has veered left of the wall and must turn right slightly to compensate
+            direction = -1
 
+        if BR - FR > .1:
+            # Robot has veered right of the wall (may soon collide) and must turn left slightly to compensate
+            direction = 1
+
+    velocity_msg.angular.z = min(direction)
     (position, rotation) = get_odom_data()
     if abs(position.x - wall_start.x) < 0.1 and abs(position.y - wall_start.y):
         # Robot has returned to where it started when it began following this wall. This 'wall' is an obstacle
@@ -223,24 +242,31 @@ def scan_360():
     return scan_data
 
 
+def right_scan():
+    """Scan the laser. Return only the values needed for hug_wall"""
+    scan = rospy.wait_for_message("scan", LaserScan, timeout=None)
+
+    return scan.ranges[0], scan.ranges[285], scan.ranges[270], scan.ranges[255]
+
+
 success = False
 attempt = 0
 velocity_msg.linear.x = 0
 velocity_msg.angular.z = 0
-pub.publish(velocity_msg) # Ensure the robot starts at rest
+pub.publish(velocity_msg)  # Ensure the robot starts at rest
 while not success:
     if attempt >= 3:
         print("This is a hard maze! I'm going to drive a bit to see if that helps.")
         drive()
     if attempt >= 6:
-        print("I've tried {a} times. I can't find a real wall and it's time to give up :(".format(a = attempt))
+        print("I've tried {a} times. I can't find a real wall and it's time to give up :(".format(a=attempt))
         print("Exiting program")
         break
     scan_data = scan_360()
     scan_start = get_odom_data()
     angle = find_wall(scan_data, attempt)
-    wall_start = go_to_wall(angle)
-    success = hug_wall(wall_start)
+    wall_start, clearance = go_to_wall(angle)
+    success = hug_wall(wall_start, clearance)
     if success:
         print("Made it out of the maze!")
         break
